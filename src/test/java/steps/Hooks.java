ad37aec48016f56ef567e7812dd7ec1f34d72d4b
Andrés Cocunubo <anfecoquin@hotmail.com>
2019-08-26 04:35:04 -0500

    Removing unused imports

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -3,12 +3,8 @@ package steps;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
-import io.cucumber.core.api.Scenario;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import sun.awt.windows.WBufferStrategy;
 
 public class Hooks {
 
